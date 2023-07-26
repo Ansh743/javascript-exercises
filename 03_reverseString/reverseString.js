8d9c78b19258c5ef0dfdbfaba8808d14fba62f62
Anshul M <anshmaske743@gmail.com>
2023-07-25 21:40:35 -0400

    03 completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+    let res = '';
+    for (let i = inputString.length-1; i >= 0; i--) {
+        const element = inputString[i];
+        res += element;
+    }
+    return res;
 };
 
 // Do not edit below this line
