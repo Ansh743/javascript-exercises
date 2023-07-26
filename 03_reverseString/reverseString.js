b0e7b1b3acc6be5f20a51221ff475c104331726c
Anshul M <anshmaske743@gmail.com>
2023-07-25 21:41:58 -0400

    03 updated - used inbuilt functions

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,10 +1,5 @@
 const reverseString = function(inputString) {
-    let res = '';
-    for (let i = inputString.length-1; i >= 0; i--) {
-        const element = inputString[i];
-        res += element;
-    }
-    return res;
+    return inputString.split("").reverse().join("");
 };
 
 // Do not edit below this line
