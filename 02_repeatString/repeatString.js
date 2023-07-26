1751c27fca9247aa31a056722e61cbed94480431
Anshul M <anshmaske743@gmail.com>
2023-07-25 21:27:49 -0400

    02_1 completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(inputString, repeats) {
+    let res = '';
+    for (let i = 0; i < repeats; i++) {
+        res += inputString;
+    }
+    return res;
 };
 
 // Do not edit below this line
