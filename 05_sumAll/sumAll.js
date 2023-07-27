41e07f7cdef82fb248a606ec1054af77d0439909
Anshul M <anshmaske743@gmail.com>
2023-07-27 00:50:34 -0400

    05 completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function(start, stop) {
+    let sum = 0;
 
+    if(start < 0 | stop < 0) return 'ERROR';
+
+    if(!(typeof start === 'number' & typeof stop === 'number')) return 'ERROR';
+
+    if(start>stop) [start, stop] = [stop, start];
+
+    for (let i = start; i <= stop; i++) sum += i;
+    return sum;
 };
 
 // Do not edit below this line
