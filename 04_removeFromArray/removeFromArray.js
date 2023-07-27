613579ceed6c2752aa4ad915ec0281e8de226c12
Anshul M <anshmaske743@gmail.com>
2023-07-27 00:04:42 -0400

    04 completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr) {
+    let res = [];
+    let args = [];
 
+    for (let i = 1; i < arguments.length; i++) args.push(arguments[i]);
+
+    arr.forEach(element => {
+        if (!(args.includes(element))) {
+            res.push(element);
+        }
+    });
+    return res;
 };
 
 // Do not edit below this line
